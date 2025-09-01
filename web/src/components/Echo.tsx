3cd34934793df8f5a87d4fac6780207454114c0e
Eric Youmans <ery@utexas.edu>
2025-09-01 01:58:17 -0400

    Fixed build issues

--- web/src/components/Echo.tsx
+++ web/src/components/Echo.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
